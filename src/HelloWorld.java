9517315eda6d3a9bbc6c508795d4ff5a548fbcb3
Laurita Tihonova <tihonova.laurita@gmail.com>
2022-11-11 21:01:48 +0200

    New line

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello Laurita");
+        System.out.println("Hello Laurita"); //Printing my name
+        //This is a comment
+        /*
+        This is a
+        multi-line
+        comment
+         */
     }
 }
---
+++ src/Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    public static void main(String[] args) {
+      //Declaring new variable of type int (integer)
+      int number;
+
+      //Initialization of number variable
+        number = 10;
+
+        //Acessing the number variables value
+        System.out.println(number);
+    }
+}

949cc5522c81c536a451e9ba1c7525a73ade0fce
Laurita Tihonova <tihonova.laurita@gmail.com>
2022-11-14 21:35:08 +0200

    Data types and Operators

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class DataTypes {
+public class DataTypes {
+    public static void main(String[] args) {
+
+        //Integer data types
+        byte num1 = 120;
+        short num2 = 10000;
+        int num3 = 234234234;
+        long num4 = 4345364635465L;
+
+        //Floating point data types
+        float num5 = 1454.3467f;
+        double num6 = 343546.6565d;
+
+        //Boolean data type
+        boolean redLightSwitch = false;
+        boolean blackLightSwitch = true;
+
+        //Character data type
+        char char1 = '%';
+        char char2 = 'y';
+        char char3 = '0';
+        char char4 = '\n'; // \n means new line - prints value in a new line
+
+        //String data type
+        String text1 = "This is very good text";
+
+        //Print out variable values
+        System.out.println(num1);
+        System.out.println(num6);
+        System.out.println(blackLightSwitch);
+        System.out.println(char4); // new line
+        System.out.println(char2);
+        System.out.println(text1);
+
+
+       /* num1 = 125;
+        System.out.println(num1);*/
+    }
 }
--- src/Operators.java
+++ src/Operators.java
@@ -1,2 +1,66 @@
-package PACKAGE_NAME;public class Operators {
+public class Operators {
+    public static void main(String[] args) {
+
+      // ASSIGNMENT OPERATORS =, +=, -=, *=, /=
+
+      int a = 100;
+        System.out.println("a = " + a);
+        a *= 10;
+        System.out.println("a = " + a);
+
+        //BASIC ARITHMETIC OPERATORS -, +, *, /, %
+
+        int x = 100;
+        x = 45+10;
+        x = 15*3;
+        System.out.println("x = " + x); //shows the new value, the latest one
+        System.out.println(x+15);
+
+        //INCREMENTATION OPERATORS & DECREMENTATION OPERATORS
+        //Post-incrementation
+        int num1 = 10;
+        num1++;
+        System.out.println("num1 = " + num1);
+        System.out.println("num1 = " + num1++);
+        System.out.println("num1 = " + num1);
+
+        //Pre-icrementation
+        System.out.println("num1 = " + --num1);
+
+        //4 ways to increase or decrease variables by one
+        num1 += 1;
+        num1 = num1 + 1;
+        num1++;
+        ++num1;
+
+        //RELATIONAL OPERATORS
+        //Equality ==
+        int i = 10;
+        int b = 10;
+        System.out.println("Equality " + (i==b));
+
+        //Inequality !=
+        System.out.println("Inequality " + (i!=b));
+
+        //Greater than > and greater than or equal to >=
+        System.out.println("Greater than " + (i > b));
+        System.out.println("Greater than or equal to " + (i >= b));
+
+        //Less than < less than or equal to <=
+        System.out.println("Less than " + (i < b));
+        System.out.println("Less than or equal to " + (i <= b));
+
+        //LOGICAL OPERATORS
+        //Logical AND - &&
+
+        boolean sun = true;
+        boolean dry = true;
+
+        System.out.println("I will go outside " +  (sun && dry) );
+
+
+
+
+
+    }
 }
